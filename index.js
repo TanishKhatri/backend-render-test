50f9c76b3543fa305a096dc78aabfc3fd118a997
Tanish Khatri <tanishkhatriofficial@gmail.com>
2026-05-21 05:08:03 +0000

    Works with all urls now

--- index.js
+++ index.js
@@ -23,19 +23,25 @@ app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
+  .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id).then((result) => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -58,23 +64,22 @@ app.post('/api/notes', (request, response) => {
 })
 
 app.put('/api/notes/:id', (request, response) => {
-  const body = request.body
-  const id = request.params.id
+  const {content, important} = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing' 
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
     })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: id
-  }
-
-  notes = notes.filter(n => n.id === id ? note : n)
-  response.json(note)
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -82,6 +87,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformed id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
