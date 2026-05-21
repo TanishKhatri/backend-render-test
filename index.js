2caf950ca90982e677f0f4e10d5683dd4bfe9183
Tanish Khatri <tanishkhatriofficial@gmail.com>
2026-05-21 03:51:48 +0000

    Changed api/id access

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const Note = require('./models/note')
-const cors = require('cors')
-app.use(cors())
+
 app.use(express.static('./dist'))
 app.use(express.json())
 const requestLogger = (request, response, next) => {
@@ -14,24 +13,6 @@ const requestLogger = (request, response, next) => {
   next()
 }
 app.use(requestLogger)
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
@@ -45,13 +26,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(n => n.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -60,14 +37,6 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0 ?
-    Math.max(...notes.map(n => Number(n.id))) :
-    0
-
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -78,14 +47,14 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
 })
 
 app.put('/api/notes/:id', (request, response) => {
